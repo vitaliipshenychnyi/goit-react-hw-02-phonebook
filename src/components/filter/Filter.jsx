de523f23c95f833618fa70c3af67856d8f544116
VitaliiPshenychnyi <117928970+vitaliipshenychnyi@users.noreply.github.com>
2023-05-24 10:42:53 +0300

    fix

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,21 +1,16 @@
-import {
-  TitleFindInput,
-  InputFindField,
-} from './Filter.styled';
+import { TitleFindInput, InputFindField } from './Filter.styled';
 
-export const Filter = ({ contacts }) => (
-  <>
-    <TitleFindInput htmlFor="">
-      Find contacts by name
-      <InputFindField
-        type="text"
-        name="find"
-        // value={this.state.find}
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-        // onChange={this.handleChange}
-      />
-    </TitleFindInput>
-  </>
+export const Filter = ({contacts}) => (
+  <TitleFindInput htmlFor="">
+    Find contacts by name
+    <InputFindField
+      type="text"
+      name="name"
+      //   value={this.state.name}
+      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+      required
+      // onChange={this.handleChange}
+    />
+  </TitleFindInput>
 );
