f4b075ce675c09f2a9ebf1144be9bde0c4b51b06
VitaliiPshenychnyi <117928970+vitaliipshenychnyi@users.noreply.github.com>
2023-05-25 08:38:14 +0300

    fix after mentor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,60 +5,61 @@ import { Filter } from './filter/Filter';
 import { Container } from './App.styled';
 
 export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+    state = {
+        contacts: [],
+        filter: '',
+    };
 
-  // функція запису отриманих даних до масиву contacts
-  formSubmit = data => {
-    if (
-      this.state.contacts.findIndex(contact => contact.name === data.name) ===
-      -1
-    ) {
-      this.state.contacts.push(data);
-    } else {
-      alert(`${data.name} is already in contacts.`);
+    // функція запису отриманих даних до масиву contacts
+    formSubmit = data => {
+        if (
+            this.state.contacts.findIndex(
+                contact => contact.name === data.name
+            ) === -1
+        ) {
+            this.state.contacts.push(data);
+        } else {
+            alert(`${data.name} is already in contacts.`);
+        }
+        this.setState({ contacts: this.state.contacts });
+    };
+
+    // функція отримання даних з поля filter
+    changeFilter = event => {
+        this.setState({ filter: event.currentTarget.value });
+    };
+
+    // функція видалення контакту зі списку
+    deleteContact = Id => {
+        const idxContact = this.state.contacts.findIndex(
+            contact => contact.id === Id
+        );
+
+        this.setState({ contact: this.state.contacts.splice(idxContact, 1) });
+    };
+
+    render() {
+        const { contacts, filter } = this.state;
+
+        // умова пошуку контактів у списку за значенням веденних даних у поле filter
+        const visibleContact = contacts.filter(contact =>
+            contact.name.toLowerCase().includes(filter.toLowerCase())
+        );
+
+        return (
+            <Container>
+                <h1>Phonebook</h1>
+                <ContactForm receiveData={this.formSubmit} />
+
+                <h2>Contacts:</h2>
+                <Filter value={filter} changeFilter={this.changeFilter} />
+                {contacts.length !== 0 && (
+                    <ContactsList
+                        contacts={visibleContact}
+                        deleteContact={this.deleteContact}
+                    />
+                )}
+            </Container>
+        );
     }
-    this.setState({ contacts: this.state.contacts });
-  };
-
-  // функція отримання даних з поля filter
-  changeFilter = event => {
-    this.setState({ filter: event.currentTarget.value });
-  };
-
-  // функція видалення контакту зі списку
-  deleteContact = event => {
-    const idxContact = this.state.contacts.findIndex(
-      contact => contact.id === event.currentTarget.dataset.id
-    );
-
-    this.setState({ contact: this.state.contacts.splice(idxContact, 1) });
-  };
-
-  render() {
-    const { contacts, filter } = this.state;
-
-    // умова пошуку контактів у списку за значенням веденних даних у поле filter
-    const visibleContact = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-
-    return (
-      <Container>
-        <h1>Phonebook</h1>
-        <ContactForm receiveData={this.formSubmit} />
-
-        <h2>Contacts:</h2>
-        <Filter value={filter} changeFilter={this.changeFilter} />
-        {contacts.length !== 0 && (
-          <ContactsList
-            contacts={visibleContact}
-            deleteContact={this.deleteContact}
-          />
-        )}
-      </Container>
-    );
-  }
 }
